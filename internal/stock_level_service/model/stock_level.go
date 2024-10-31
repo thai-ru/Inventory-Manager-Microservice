e66b31920efbc7172fdb6bba15ffa3103f3999c0
thai-ru <robinthairu34@gmail.com>
2024-10-31 23:26:17 +0300

    stock model

--- internal/stock_level_service/model/stock_level.go
+++ internal/stock_level_service/model/stock_level.go
@@ -1,1 +1,16 @@
 package model
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type StockLevel struct {
+	gorm.Model
+	ProductID    string
+	Quantity     int64
+	MinThreshold int64
+	MaxThreshold int64
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+}
